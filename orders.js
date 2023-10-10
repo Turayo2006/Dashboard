bab68afc535aed22f120a35b63674032bed2fad5
Turayo2006 <120662177+Turayo2006@users.noreply.github.com>
2023-10-10 10:34:09 +0100

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,1 +1,35 @@
+const Orders = [
+    
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '23456',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'Lavender K3456 Drone',
+    productNumber: '36543',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+},
+{
+    productName: 'Ruko F11 Drone',
+    productNumber: '43256',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'Drone with camera Drone',
+    productNumber: '26998',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'GPS 4K Drone',
+    productNumber: '221821',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+}
+
+]
 
